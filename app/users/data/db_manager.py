7f94c435d52720d550ead1b91940dbd6fe76f4c3
Lance Makabali <lmm1182@usnh.edu>
2025-09-15 23:58:00 -0400

    database complete, new update of progress

--- app/users/data/db_manager.py
+++ app/users/data/db_manager.py
@@ -1,4 +1,4 @@
-# from bson import ObjectId
+from bson import ObjectId
 import pymongo
 from pydantic import BaseModel
 
@@ -12,14 +12,8 @@ class DBManager:
         self.col = mydb[col]
 
     def create(self, d: dict):
-        '''create user
-        
-        args:
-            d (dict): data to insert into db
-        
-        returns:
-            id (str): object_id as a str
-        '''
+        result = self.col.insert_one(d)
+        return str(result.inserted_id)
 
     def read_by_id(self, obj_id:str):
         '''read user by id 
@@ -30,6 +24,10 @@ class DBManager:
         returns:
             data (dict): data retrieved, or None
         '''
+        result = self.col.find_one({"_id": ObjectId(obj_id)})
+        if result:
+            result["_id"] = str(result["_id"])
+        return result
         
     def read(self,query:dict):
         '''read many by query
@@ -40,6 +38,11 @@ class DBManager:
         returns:
             data (list): convert Cursor to list for return
         '''
+        cursor = self.col.find(query)
+        results = list(cursor)
+        for r in results:
+            r["_id"] = str(r["_id"])
+        return results
     
     def read_all(self):
         '''read all
@@ -47,6 +50,11 @@ class DBManager:
         returns:
             data (list)
         '''
+        cursor = self.col.find({})
+        results = list(cursor)
+        for r in results:
+            r["_id"] = str(r["_id"])
+        return results
 
     def update(self,obj_id,updates:dict):
         ''' update by id 
@@ -58,6 +66,11 @@ class DBManager:
         returns:
             modified_count (int)
         '''
+        result = self.col.update_one(
+            {"_id": ObjectId(obj_id)},
+            {"$set": updates}
+        )
+        return result.modified_count
 
     def delete_by_id(self,obj_id):
         ''' delete by id
@@ -68,6 +81,8 @@ class DBManager:
         returns:
             deleted_count (int)
         '''
+        result = self.col.delete_one({"_id": ObjectId(obj_id)})
+        return result.deleted_count
     
     def delete(self,query:dict):
         ''' delete many by query 
@@ -78,6 +93,8 @@ class DBManager:
         returns:
             deleted_count (int)
         '''
+        result = self.col.delete_many(query)
+        return result.deleted_count
     
     def delete_all(self):
         ''' delete all
